53df1ca7776b83e2e3208ac21ed00d2adbad5513
Pyee <pikyee@apptivitylab.com>
2016-07-16 18:12:20 +0800

    Added PhotographerProfileVC and update layout from Storyboard

---
+++ Photograph/PhotographerProfileVC.swift
@@ -0,0 +1,26 @@
+//
+//  PhotographerProfileVC.swift
+//  Photograph
+//
+//  Created by Pyee on 16/07/2016.
+//  Copyright © 2016 Kingsley Tan. All rights reserved.
+//
+
+import UIKit
+
+class PhotographerProfileVC: UIViewController {
+    @IBOutlet weak var photographerImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artworkCollectionView: UICollectionViewCell!
+    
+    @IBOutlet weak var commentsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
