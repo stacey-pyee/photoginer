d0f0aa3e9ffd5f3dd0d5e2ae9c27d4d1c5897925
Pyee <pikyee@apptivitylab.com>
2016-07-20 09:37:48 +0800

    Added empty model classes: Photographer, Customer, Package, Service, Rating

---
+++ Photograph/Photographer.swift
@@ -0,0 +1,9 @@
+//
+//  Photographer.swift
+//  Photograph
+//
+//  Created by Pyee on 20/07/2016.
+//  Copyright © 2016 Kingsley Tan. All rights reserved.
+//
+
+import Foundation
---
+++ Photograph/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  Photograph
+//
+//  Created by Pyee on 20/07/2016.
+//  Copyright © 2016 Kingsley Tan. All rights reserved.
+//
+
+import Foundation
