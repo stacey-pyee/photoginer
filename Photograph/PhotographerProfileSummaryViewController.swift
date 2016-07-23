57b34fc96a7efc12b13a545b5b77558b05d9d8a2
Kingsley <GraceLow@Graces-MacBook-Pro.local>
2016-07-23 15:37:17 +0800

    Added PhotographerProfileSummaryVC - This is a VC to show a list of photographers

---
+++ Photograph/PhotographerProfileSummaryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  Photographer Profile Summary.swift
+//  Photograph
+//
+//  Created by Grace Low on 7/23/16.
+//  Copyright © 2016 Kingsley Tan. All rights reserved.
+//
+
+import UIKit
+
+class PhotographerProfileSummaryViewController: UIViewController {
+    @IBOutlet weak var photographerImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var serviceSelected: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
