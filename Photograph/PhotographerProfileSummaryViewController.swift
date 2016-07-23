6816ad16e4b95a1f5054791158ae1190c42525b2
Kingsley <GraceLow@Graces-MacBook-Pro.local>
2016-07-23 16:34:23 +0800

    1. Created dummy data for 4 photographers 2. Created Data source and delegate for tableView

--- Photograph/PhotographerProfileSummaryViewController.swift
+++ Photograph/PhotographerProfileSummaryViewController.swift
@@ -15,22 +15,25 @@ class PhotographerProfileSummaryViewController: UIViewController {
     @IBOutlet weak var cell: UITableViewCell!
     @IBOutlet weak var PhotographerTableView: UITableView!
     
-    var photographer1: Photographer = Photographer(name: "Adam", contact: "0109999991", yearOfExperience: 3)
-    var photographer2: Photographer = Photographer(name: "Bob", contact: "0109999992", yearOfExperience: 5)
-    var photographer3: Photographer = Photographer(name: "Catherine", contact: "0109999993", yearOfExperience: 1)
-    var photographer4: Photographer = Photographer(name: "Jane", contact: "0109999994", yearOfExperience: 10)
-
-    
-    
+    var photographerList: [Photographer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var photographerList: [Photographer] = [photographer1, photographer2, photographer3, photographer4]
+        self.createDummyData()
+        self.PhotographerTableView.dataSource = self
+        self.PhotographerTableView.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func createDummyData() {
+        var photographer1: Photographer = Photographer(name: "Adam", contact: "0109999991", yearOfExperience: 3)
+        var photographer2: Photographer = Photographer(name: "Bob", contact: "0109999992", yearOfExperience: 5)
+        var photographer3: Photographer = Photographer(name: "Catherine", contact: "0109999993", yearOfExperience: 1)
+        var photographer4: Photographer = Photographer(name: "Jane", contact: "0109999994", yearOfExperience: 10)
+        photographerList = [photographer1, photographer2, photographer3, photographer4]
     }
 }
 
@@ -38,8 +41,17 @@ extension PhotographerProfileSummaryViewController: UITableViewDataSource{
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        let photographerName: String = self.photographerList[indexPath.row].name
+        cell.textLabel?.text = photographerName
+        return cell
+    }
 }
 
 extension PhotographerProfileSummaryViewController: UITableViewDelegate{
-    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let photographerProfileVC: PhotographerProfileViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PhotographerProfileViewController") as! PhotographerProfileViewController
+        self.navigationController?.pushViewController(photographerProfileVC, animated: true)
+    }
 }
