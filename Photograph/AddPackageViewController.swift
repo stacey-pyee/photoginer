11aa5bc2bb84e620bf2ab5661a37ce7219c081b3
Pyee <pikyee@apptivitylab.com>
2016-08-02 13:03:53 +0800

    Added view for AddPackageViewController with basic code setup

--- Photograph/AddPackageViewController.swift
+++ Photograph/AddPackageViewController.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+class AddPackageViewController: UIViewController {
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var serviceTextField: UITextField!
+    
+    @IBAction func cancelServiceCreation(sender: UIButton) {
+        self.presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func saveService(sender: UIButton) {
+    }
+}
