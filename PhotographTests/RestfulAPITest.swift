25d7fbb3fc2bc06bd7056b1272e931c5e34413fb
Pyee <pikyee@apptivitylab.com>
2016-08-09 20:58:32 +0800

    Added test case creating photo package and save to Backendless

--- PhotographTests/RestfulAPITest.swift
+++ PhotographTests/RestfulAPITest.swift
@@ -99,6 +99,41 @@ class RestfulAPITest: XCTestCase {
         postTask.resume()
         self.waitForExpectationsWithTimeout(100, handler: nil)
     }
+    
+    func testCreatingPackageToAPI() {
+        let expectation = self.expectationWithDescription("Create Package")
+        
+        let url: NSURL = NSURL(string: "https://api.backendless.com/v1/data/PhotoPackages")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "POST"
+        
+        // Create PhotoPackage object
+        let photographer: Photographer = Photographer(name: "Magdalene", contact: "018-98765432", yearOfExperience: 2)
+        let package: PhotoPackage = PhotoPackage(name: "Holiday", price: 130.00, photographer: photographer, photoServiceType: PhotoServiceType.Candid)
+        // Change it as a JSONObject
+        let jsonDictionary: [String : AnyObject] = [
+            "name" : package.name,
+            "price" : package.price
+        ]
+        
+        let jsonData: NSData = try! NSJSONSerialization.dataWithJSONObject(jsonDictionary, options: NSJSONWritingOptions(rawValue: 0))
+        urlRequest.HTTPBody = jsonData
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("REST", forHTTPHeaderField: "application-type")
+        
+        let postTask: NSURLSessionDataTask = self.session.dataTaskWithRequest(urlRequest) { (data: NSData?, response: NSURLResponse?, error:NSError?) in
+            
+            let responseInString: String? = String(data: data!, encoding: NSUTF8StringEncoding)
+            NSLog("\(responseInString)")
+            
+            
+            expectation.fulfill()
+        }
+        
+        postTask.resume()
+        self.waitForExpectationsWithTimeout(100, handler: nil)
+    }
+
 
     func testExample() {
         // This is an example of a functional test case.
