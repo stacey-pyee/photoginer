384cf27556d66c3aba5258ddd6d00e9fcfcd23d0
Pyee <pikyee@apptivitylab.com>
2016-08-09 21:21:02 +0800

    Added PhotoPackageLoader class which is a singleton

---
+++ Photograph/PhotoPackageLoader.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPackageLoader.swift
+//  Photograph
+//
+//  Created by Pyee on 09/08/2016.
+//  Copyright © 2016 Kingsley Tan. All rights reserved.
+//
+
+import Foundation
+
+class PhotoPackageLoader {
+    // Singleton pattern
+    static let sharedLoader: PhotoPackageLoader = PhotoPackageLoader()
+    private init() {
+        
+    }
+}
