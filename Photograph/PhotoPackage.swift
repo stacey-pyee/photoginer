e72c462d5ba51c58cbac3b424699491a8db792e7
Pyee <pikyee@apptivitylab.com>
2016-08-11 15:14:49 +0800

    Add initialisation with JSONObject

--- Photograph/PhotoPackage.swift
+++ Photograph/PhotoPackage.swift
@@ -8,17 +8,29 @@
 
 import Foundation
 
-class PhotoPackage {
+class PhotoPackage: NSObject {
     var name: String
     var price: Double
     var photographer: Photographer
     var photoServiceType: PhotoServiceType
-    var description: String?
+    //var description: String?
+    var objectId: String?
     
     init(name: String, price: Double, photographer: Photographer, photoServiceType: PhotoServiceType) {
         self.name = name
         self.price = price
         self.photographer = photographer
         self.photoServiceType = photoServiceType
+    }
+}
+
+extension PhotoPackage {
+    convenience init?(jsonDictionary: [String: AnyObject]) {
+        let daniel = Photographer(name: "Daniel", contact: "012-9876543", yearOfExperience: 2)
+        let name = jsonDictionary["name"] as? String
+        let price = jsonDictionary["price"] as? Double
+        let objectId: String = (jsonDictionary["objectId"] as? String)!
+        
+        self.init(name: name!, price: price!, photographer: daniel, photoServiceType: PhotoServiceType.Wedding)
     }
 }
